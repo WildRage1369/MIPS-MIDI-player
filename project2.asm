f693a4ccb72ba193a2c3b0520185c7421db7161f
Maxine Reyes Gomez <rg.maxine100@gmail.com>
2024-09-20 14:42:18 -0400

    parse_header: Change algorithm to work with new read_bytes procedure and fix bug in read_bytes

--- project2.asm
+++ project2.asm
@@ -16,17 +16,16 @@ while_loop:
     li $a1, 4                    # how many bytes to read
     jal read_bytes               # call read_bytes
     lw $s0, ($v0)                # load word
+    addi $gp, $gp, -4            # deallocate space on heap
 
     beq $s0 $zero, end_of_file   # goto end_of_file if at eof
     move $a0, $s7                # load file descriptor
 
     li $s1, 0x4d546864           # load Header Chunk id
-    bne $s1, $s0, cond_1_f       # branch if equal: goto parse_header
-    la $s6, parse_header
-    jalr $s6
-    addi $sp, $sp, -8            # allocate space for return values
-    sw $v0, 0($sp)               # store track quantity
-    sw $v1, 4($sp)               # store time division
+    bne $s1, $s0, cond_1_f       # branch if not equal: skip header
+    jal parse_header
+    move $s2, $v0                # store track quantity
+    move $s3, $v1               # store time division
 cond_1_f:
 
     j while_loop
@@ -42,33 +41,46 @@ end_of_file:
 # $v0 = track quantity
 # $v1 = time division
 parse_header:
-    move $s7, $a0                # load file descriptor
+    # Store $s* in stack to be restored
+    addi $sp, $sp, -12
+    sw $s0, 0($sp)
+    sw $s7, 4($sp)
+    sw $ra, 8($sp)
+
+    # load arguments
+    move $s7, $a0
 
     # Read in chunk size
+    move $a0, $s7
+    li $a1, 4                    # how many bytes to read
+    jal read_bytes
+    lw $s0, ($v0)                # load word into $s0
+    addi $gp, $gp, -4            # deallocate space on heap
 
-    # Read in track quantity and time division
-    li $v0, 14                   # load read file system call
-    move $a0, $s7                # load file descriptor
-    sub $sp, $sp, $s0            # allocate space for etc buffer
-    la $a1, 0($sp)               # load etc buffer
-    move $a2, $s0                # max input size (6 bytes)
-    syscall                      # read file syscall
-
-    lw $s0, 0($sp)               # load etc buffer data into $s0
-    addi $sp, $sp, 4             # deallocate etc buffer
+    # Read in format type, track quantity and time division
+    move $a0, $s7                
+    move $a1, $s0                # how many bytes to read
+    jal read_bytes
+    lw $t1, 0($v0)               # load word into $t1
+    sub $gp, $gp, $s0            # deallocate space on heap
 
     # return track quantity and time division
-    andi $v0, $s0, 0x00FF00      # mask out track quantity
-    andi $v1, $s0, 0x0000FF      # mask out time division
+    andi $v0, $t1, 0xFFFF0000    # mask out track quantity
+    srl $v0, $v0, 16             # shift track quantity to the right
+    andi $v1, $t1, 0x0000FFFF    # mask out time division
+
+    lw $s7, 0($sp)               # restore $s7
+    lw $s0, 4($sp)               # restore $s0
+    lw $ra, 8($sp)               # restore $ra
+    addi $sp, $sp, 12            # deallocate stack
     jr $ra                       # return
 
 # $a0 = file descriptor
-# $v0 = word
 # $a1 = amount of bytes to read
 # $v0 = address of last byte on heap
 read_bytes:
-    # Store $s* in stack to be restored
-    addi $sp, $sp, -28
+    # Store $s* and $ra in stack to be restored
+    addi $sp, $sp, -32
     sw $s0, 0($sp)
     sw $s1, 4($sp)
     sw $s3, 8($sp)
@@ -76,6 +88,7 @@ read_bytes:
     sw $s5, 16($sp)
     sw $s6, 20($sp)
     sw $s7, 24($sp)
+    sw $ra, 28($sp)
 
     move $s7, $a0                # load file descriptor
     move $s6, $a1                # store byte amount in $s6 to use as counter in loop
@@ -106,7 +119,8 @@ rb_loop:
     j rb_loop
 
 rb_end:
-    la $v0, ($s1)             # Return address to last byte on heap
+    addi $sp $sp, 4              # deallocate space on stack
+    move $v0, $s1                # Return address to last byte on heap
 
     # Restore $s0 and $s1
     lw $s0, 0($sp)
@@ -116,6 +130,7 @@ rb_end:
     lw $s5, 16($sp)
     lw $s6, 20($sp)
     lw $s7, 24($sp)
-    addi $sp, $sp, 28
+    lw $ra, 28($sp)
+    addi $sp, $sp, 32
 
     jr $ra                       # Return
